3454f92ae0584c1a5737c5808df2b9b06db49d54
Baiju Muthukadan <baiju.m.mail@gmail.com>
2015-06-03 01:04:39 +0530

    Database schema

---
+++ scripts/db/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS "member" CASCADE;
+
+CREATE TABLE "member" (
+       id serial PRIMARY KEY,
+       username TEXT UNIQUE NOT NULL,
+       email TEXT UNIQUE NOT NULL,
+       password BYTEA NOT NULL,
+       salt BYTEA NOT NULL,
+       fullname TEXT NOT NULL,
+       created_at TIMESTAMP DEFAULT now(),
+       update_at TIMESTAMP DEFAULT now()
+);
+
+DROP TABLE IF EXISTS "doc" CASCADE;
+
+CREATE TABLE "doc" (
+       id serial PRIMARY KEY,
+       link TEXT NOT NULL,
+       source TEXT NOT NULL,
+       created_at TIMESTAMP DEFAULT now(),
+       update_at TIMESTAMP DEFAULT now(),
+       member_id BIGINT REFERENCES member (id)
+);
